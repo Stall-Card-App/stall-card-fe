a27957dc56b3c2b4a2540a26b9d36d0c13ee4132
John Kiernan <jkiernan12@yahoo.com>
2022-02-05 11:02:24 -0500

    Add schedule

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import Sidebar from '../Sidebar/Sidebar';
 import cloudy from '../assets/cloudy.svg';
 import './Dashboard.scss';
+import Schedule from '../Schedule/Schedule';
 
 function Dashboard() {
   return (
@@ -12,10 +13,7 @@ function Dashboard() {
           <h3>Today's Weather</h3>
           <img src={cloudy} />
         </section>
-        <section className='schedule'>
-          <h3>Today's Weather</h3>
-          <img src={cloudy} />
-        </section>
+        <Schedule />
         <section className='horse-overview'>
           <h3>Today's Weather</h3>
           <img src={cloudy} />
---
+++ src/Schedule/Schedule.js
@@ -0,0 +1,37 @@
+import './Schedule.scss'
+
+function Schedule() {
+  return ( 
+    <section className='schedule'>
+          <h3>Schedule</h3>
+          <table>
+            <tr>
+              <td>9:00am</td>
+              <td>Appointment here</td>
+            </tr>
+            <tr>
+              <td>9:00am</td>
+              <td>Appointment here</td>
+            </tr>
+            <tr>
+              <td>9:00am</td>
+              <td>Appointment here</td>
+            </tr>
+            <tr>
+              <td>9:00am</td>
+              <td>Appointment here</td>
+            </tr>
+            <tr>
+              <td>9:00am</td>
+              <td>Appointment here</td>
+            </tr>
+            <tr>
+              <td>9:00am</td>
+              <td>Appointment here</td>
+            </tr>
+          </table>
+        </section>
+   );
+}
+
+export default Schedule;
