b3c1b42094e171a8f93c4fadc517e2be7a90c6bc
John Kiernan <jkiernan12@yahoo.com>
2022-02-05 10:41:08 -0500

    Add basic grid layout for dashboard

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,6 +1,27 @@
+import Sidebar from '../Sidebar/Sidebar';
+import cloudy from '../assets/cloudy.svg';
+import './Dashboard.scss';
+
 function Dashboard() {
   return (
-    <p>DashBoard</p>
+    <>
+      <Sidebar />
+      <main className='Dashboard'>
+        <header>Header here</header>
+        <section className='weather-widget'>
+          <h3>Today's Weather</h3>
+          <img src={cloudy} />
+        </section>
+        <section className='schedule'>
+          <h3>Today's Weather</h3>
+          <img src={cloudy} />
+        </section>
+        <section className='horse-overview'>
+          <h3>Today's Weather</h3>
+          <img src={cloudy} />
+        </section>
+      </main>
+    </>
   );
 }
 
