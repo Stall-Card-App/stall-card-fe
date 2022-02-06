bb3cf0061e6d0052d231ee5eed7f0d66f5e6be6a
John Kiernan <jkiernan12@yahoo.com>
2022-02-06 09:29:06 -0500

    Add multi pages to form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -24,6 +24,7 @@ const initialInputsState = {
 
 function Form() {
   const [inputs, setInputs] = useState(initialInputsState)
+  const [formPage, setFormPage] = useState(1)
 
   const handleInputChange = (e) => {
     e.preventDefault();
@@ -36,13 +37,25 @@ function Form() {
     e.preventDefault();
     console.log(inputs);
     setInputs(initialInputsState)
+    MicroModal.close('modal-1')
+    setFormPage(() => 1)
+  }
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    setFormPage(() => formPage + 1);
+  }
+
+  const handleBack = (e) => {
+    e.preventDefault();
+    setFormPage(() => formPage - 1);
   }
 
   MicroModal.init();
   return ( 
     <div className='modal' id="modal-1" aria-hidden="true">
       <div tabIndex="-1" data-micromodal-close>
-        <div className='modal-content' role="dialog" aria-modal="true" aria-labelledby="modal-1-title" >
+        {formPage === 1 && <div className='modal-content' role="dialog" aria-modal="true" aria-labelledby="modal-1-title" >
           <header className='modal-header'>
             <h2 id="modal-1-title">
               Horse Info
@@ -103,10 +116,93 @@ function Form() {
                 <label htmlFor="blanketing_temp">Blanketing Temp</label>
                 <input id="blanketing_temp" type="text" value={inputs.blanketing_temp} onChange={(e) => handleInputChange(e)} />
               </div>
-              <button className='submit-button' onClick={(e) => handleSubmit(e)}>Submit</button>
+              <button className='next-button' onClick={(e) => handleNext(e)}>Next</button>
+            </form>
+          </div>
+        </div>}
+        {formPage === 2 && <div className='modal-content' role="dialog" aria-modal="true" aria-labelledby="modal-1-title" >
+          <header className='modal-header'>
+            <h2 id="modal-1-title">
+              Care Info
+            </h2>
+            <button className='modal-exit' aria-label="Close modal" data-micromodal-close>x</button>
+          </header>
+          <div id="modal-1-content">
+            <form>
+              <div className="input-container">
+                <label htmlFor="am_feed">AM Feed</label>
+                <input id="am_feed" type="text" value={inputs.am_feed} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="pm_feed">PM Feed</label>
+                <input id="pm_feed" type="text" value={inputs.pm_feed} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="supplements">supplements</label>
+                <input id="supplements" type="text" value={inputs.supplements} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="turnout">Turnout</label>
+                <input id="turnout" type="text" value={inputs.turnout} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="blanketing_temp">Blanketing Temp</label>
+                <input id="blanketing_temp" type="text" value={inputs.blanketing_temp} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <button className='back-button' onClick={(e) => handleBack(e)}>Back</button>
+              <button className='next-button' onClick={(e) => handleNext(e)}>Next</button>
             </form>
           </div>
         </div>
+        }
+        {formPage === 3 &&         <div className='modal-content' role="dialog" aria-modal="true" aria-labelledby="modal-1-title" >
+          <header className='modal-header'>
+            <h2 id="modal-1-title">
+              Contact Info
+            </h2>
+            <button className='modal-exit' aria-label="Close modal" data-micromodal-close>x</button>
+          </header>
+          <div id="modal-1-content">
+            <form>
+            <div className="input-container">
+                <label htmlFor="owner_id">Owner</label>
+                <select id="owner_id" onChange={(e) => handleInputChange(e)}>
+                  <option disabled selected value> select an option </option>
+                  <option>New owner</option>
+                  <option value="id here">Owner name 1</option>
+                  <option value="another id here">Owner name 2</option>
+                  <option value="id">Owner name 3</option>
+                </select>
+                  <div className="input-container">
+                    <label htmlFor="supplements">supplements</label>
+                    <input id="supplements" type="text" value={inputs.supplements} onChange={(e) => handleInputChange(e)} />
+                  </div>
+                  <div className="input-container">
+                    <label htmlFor="supplements">supplements</label>
+                    <input id="supplements" type="text" value={inputs.supplements} onChange={(e) => handleInputChange(e)} />
+                  </div>
+              </div>
+              <div className="input-container">
+                <label htmlFor="pm_feed">PM Feed</label>
+                <input id="pm_feed" type="text" value={inputs.pm_feed} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="supplements">supplements</label>
+                <input id="supplements" type="text" value={inputs.supplements} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="turnout">Turnout</label>
+                <input id="turnout" type="text" value={inputs.turnout} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <div className="input-container">
+                <label htmlFor="blanketing_temp">Blanketing Temp</label>
+                <input id="blanketing_temp" type="text" value={inputs.blanketing_temp} onChange={(e) => handleInputChange(e)} />
+              </div>
+              <button className='back-button' onClick={(e) => handleBack(e)}>Back</button>
+              <button className='submit-button' onClick={(e) => handleSubmit(e)}>Submit</button>
+            </form>
+          </div>
+        </div>}
       </div>
     </div>
    );
