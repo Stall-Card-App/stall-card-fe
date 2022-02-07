eb4614ee40c8a2dc211b7d4fe2cbd7d9c92d65c2
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-02-06 22:39:50 -0500

    Reset default styling in App

--- src/App/App.js
+++ src/App/App.js
@@ -2,15 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import AllHorses from '../AllHorses/AllHorses';
 import Dashboard from '../Dashboard/Dashboard';
 import HorseProfile from '../HorseProfile/HorseProfile';
-import './App.css';
+import Sidebar from '../Sidebar/Sidebar';
+import './App.scss';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Dashboard />} />
-      <Route path='/horses' element={<AllHorses />} />
-      <Route path='/horses/:id' element={<HorseProfile />} />
-    </Routes>
+    <main>
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/horses' element={<AllHorses />} />
+        <Route path='/horses/:id' element={<HorseProfile />} />
+      </Routes>
+    </main>
+
   );
 }
 
