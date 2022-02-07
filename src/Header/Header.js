bae403b03613e9c327ab320fd15a94984505279f
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-02-06 22:45:01 -0500

    Add basic JSX skeleton for Header component

---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.scss'
+
+function Header() {
+    return(
+        <header>
+            <h1>Dashboard</h1>
+            <p>Welcome, Admin</p>
+            <span>
+                <img />
+                <p>profile</p>
+            </span>
+        </header>
+    )
+}
+
+export default Header;
