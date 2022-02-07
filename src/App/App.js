de7188786002db2b72e587d73cf06fe1bf73c976
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-02-06 23:01:25 -0500

    Add main section styling

--- src/App/App.js
+++ src/App/App.js
@@ -3,17 +3,22 @@ import AllHorses from '../AllHorses/AllHorses';
 import Dashboard from '../Dashboard/Dashboard';
 import HorseProfile from '../HorseProfile/HorseProfile';
 import Sidebar from '../Sidebar/Sidebar';
+import Header from '../Header/Header';
 import './App.scss';
 
 function App() {
   return (
     <main>
       <Sidebar />
-      <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/horses' element={<AllHorses />} />
-        <Route path='/horses/:id' element={<HorseProfile />} />
-      </Routes>
+      <section className='main-section'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/horses' element={<AllHorses />} />
+          <Route path='/horses/:id' element={<HorseProfile />} />
+        </Routes>
+      </section>
+
     </main>
 
   );
