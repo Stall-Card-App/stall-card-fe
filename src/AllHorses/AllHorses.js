e47a46fda7d6778f871bcc079232309e1efcf59e
August Reid <c.august.reid@gmail.com>
2022-02-12 16:40:28 -0700

    Create loading component with horse gif

--- src/AllHorses/AllHorses.js
+++ src/AllHorses/AllHorses.js
@@ -3,19 +3,21 @@ import "./AllHorses.scss";
 import { Link } from 'react-router-dom';
 import mockData from "../mockData";
 import HorseCard from "../HorseCard/HorseCard";
+import Loading from "../Loading/Loading.js";
 import { fetchAllHorses } from '../graphqlQueries.js';
 import { useQuery } from '@apollo/client';
 
 function AllHorses() {
   const [allHorses, setAllHorses] = useState([])
 
-  const { data } = useQuery(fetchAllHorses, {
+  const { data, loading, error } = useQuery(fetchAllHorses, {
     onCompleted: data => {
       setAllHorses(() => data.fetchHorses)
     }})
 
   return (
     <div className="horse-grid">
+      {loading && <Loading />}
       {allHorses?.length > 0 && allHorses.map((horse) => {
         return <Link to={`/horses/${horse.id}`} key={horse.id}>
         <HorseCard 
---
+++ src/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import loadingGif from "../assets/horsierun.gif";
+
+const Loading = () => {
+    return (
+        <div>
+            <img src={loadingGif} alt="animation of running horse"/>
+            <p>Loading, please wait...</p>
+        </div>
+    )
+}
+
+export default Loading;
