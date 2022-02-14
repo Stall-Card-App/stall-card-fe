ca0f350344372e2b1b4941f847575699d3a307c6
Eric Wang <87143658+ewang0@users.noreply.github.com>
2022-02-13 20:55:07 -0500

    Add event handlers for NavLinks

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 import "./Sidebar.scss"
 
-function Sidebar() {
+function Sidebar({ updateCurrentPage }) {
   return ( 
     <aside className='sidebar-container'>
       <div className='placeholder-logo'>
@@ -11,13 +11,13 @@ function Sidebar() {
       <nav>
         <ul className='nav-items'>
           <li>
-            <NavLink to="/"><img src="home.svg" alt="home icon"/>Home</NavLink>
+            <NavLink to="/" onClick={() => {updateCurrentPage('Dashboard')}}><img src="home.svg" alt="home icon"/>Home</NavLink>
           </li>
           <li>
-            <NavLink to="/horses"><img src="grid.svg" alt="grid icon"/>All Horses</NavLink>
+            <NavLink to="/horses" onClick={() => {updateCurrentPage('All Horses')}}><img src="grid.svg" alt="grid icon"/>All Horses</NavLink>
           </li>
           <li>
-            <NavLink to="/"><img src="calendar.svg" alt="home icon"/>Schedule</NavLink>
+            <NavLink to="/" onClick={() => {updateCurrentPage('Dashboard')}}><img src="calendar.svg" alt="home icon"/>Schedule</NavLink>
           </li>
         </ul>
       </nav>
