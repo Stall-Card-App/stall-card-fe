0f5d71bb651d1f3a64d7f19d6b56dfa5193ede22
August Reid <c.august.reid@gmail.com>
2022-02-14 18:18:35 -0700

    Fetch weather forecast

--- src/WeatherWidget/WeatherWidget.js
+++ src/WeatherWidget/WeatherWidget.js
@@ -1,13 +1,26 @@
 import './WeatherWidget.scss';
 import cloudy from '../assets/cloudy.svg';
+import { useState, useEffect } from "react";
+import icon01d from "../assets/weatherIcons/01d.png"
 
 function WeatherWidget() {
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    fetch("https://aqueous-savannah-80171.herokuapp.com/forecasts?location=denver, co")
+    .then(response => response.json())
+    .then(data => {console.log(data) 
+      setForecast(data)})
+    // .then(console.log(forecast, "state"))
+  },[])
+console.log(forecast, "state");
+
   return ( 
     <section className='weather-widget'>
     <h3>Today's Weather</h3>
     <div className='weather-widget-content'>
       <div className='heading'>
-        <img src={cloudy} />
+        <img src={icon01d} />
         <div className='current-container'>
           <p className='current-temp'> 77°F</p>
           <p className='current-conditions'>Cloudy</p>
@@ -16,7 +29,7 @@ function WeatherWidget() {
       <div className='hourly-weather-container'>
         <article className='hourly-weather'>
           <p>9AM</p>
-          <img src={cloudy} />
+          <img src={icon01d} />
           <p>77°</p>
         </article>
         <article className='hourly-weather'>
