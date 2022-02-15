84238a71324ee1720f45d5d88c10103507973fdd
August Reid <c.august.reid@gmail.com>
2022-02-14 21:51:40 -0700

    Add am/pm to hourly weather

--- src/HourWeather/HourWeather.js
+++ src/HourWeather/HourWeather.js
@@ -3,16 +3,17 @@ import "./HourWeather.scss";
 import weatherIcons from "../WeatherWidget/icons.js";
 
 const HourWeather = ({time, temperature, conditions, icon}) => {
-
-    const hour = new Date(time).toLocaleString('en-US', {
+    const date = new Date(time).toLocaleString('en-US', {
         hour: 'numeric',
         minute: 'numeric',
         hour12: true
     })
+    const hour = date.split(":");
+    const amPm = hour[1].split(" ");
  
     return (
         <article className='hourly-weather'>
-            <p>{hour}</p>
+            <p>{`${hour[0]}${amPm[1]}`}</p>
             <img src={weatherIcons[icon]} alt={`Icon for ${conditions}`}/>
             <p>{`${Math.round(temperature)}°`}</p>
          </article>
--- src/WeatherWidget/WeatherWidget.js
+++ src/WeatherWidget/WeatherWidget.js
@@ -41,46 +41,6 @@ function WeatherWidget() {
             />
           )
         })}
-        {/* <article className='hourly-weather'>
-          <p>9AM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>10AM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>11AM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>12PM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>1PM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>2PM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>3PM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article>
-        <article className='hourly-weather'>
-          <p>4PM</p>
-          <img src={cloudy} />
-          <p>77°</p>
-        </article> */}
       </div>
     </div>
   </section>}
