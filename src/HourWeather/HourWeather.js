a9c926086ca8af6a07b3e4ead9ef38115084f6e2
August Reid <c.august.reid@gmail.com>
2022-02-14 21:26:06 -0700

    Reformat time

--- src/HourWeather/HourWeather.js
+++ src/HourWeather/HourWeather.js
@@ -3,9 +3,16 @@ import "./HourWeather.scss";
 import weatherIcons from "../WeatherWidget/icons.js";
 
 const HourWeather = ({time, temperature, conditions, icon}) => {
+
+    const hour = new Date(time).toLocaleString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    })
+ 
     return (
         <article className='hourly-weather'>
-            <p>{time}</p>
+            <p>{hour}</p>
             <img src={weatherIcons[icon]} alt={`Icon for ${conditions}`}/>
             <p>{`${Math.round(temperature)}°`}</p>
          </article>
